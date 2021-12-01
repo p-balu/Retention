a2c0765f1f06340b1543e1df2defe04e475d8def
Balachanderrao <palepubalachanderrao25@gmail.com>
2021-12-01 01:55:28 -0500

    initial setup

---
+++ src/components/TextField.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const TextInput = ({
+  name,
+  type,
+  placeholder,
+  onChange,
+  className,
+  value,
+  children,
+  label,
+  ...props
+}) => {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        className={className}
+      />
+    </>
+  );
+};
+
+TextInput.defaultProps = {
+  type: "text",
+  className: "",
+};
+
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+};
